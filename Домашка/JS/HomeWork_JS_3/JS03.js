3d65cc6d948d0818ad36dd975ecfb235ba5acafe
MykolaNovikov <mykolanovikov91@gmail.com>
2023-04-22 09:28:42 +0300

    fixed String: capitalize and String: youtube

--- Домашка/JS/HomeWork_JS_3/JS03.js
+++ Домашка/JS/HomeWork_JS_3/JS03.js
@@ -15,7 +15,7 @@
         let str = "cANBerRa";
         let result;
         let firstLetter = str[0].toUpperCase();
-        let otherLetters = str.slice(-(str.length - 1)).toLowerCase();
+        let otherLetters = str.slice(1).toLowerCase();
         result  = firstLetter + otherLetters;
         console.log(result);
     }
@@ -81,8 +81,10 @@
    
     // String: youtube 
 
+    {
     const  youTube = /https:\/\/(?:youtu\.be\/|(?:[a-z]{2,3}\.)?youtube\.com\/watch(?:\?|#\!)v=)([\w-]{11}).*/gi;
     let str = prompt('Вставте посилання на бажанне відео з ютубу');
     let found = str.match(youTube);
     let http = found [1];
-    document.write('<iframe width="560" height="315" src="https://www.youtube.com/embed/ ${http}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>')
+    document.write(`<iframe width="560" height="315" src="https://www.youtube.com/embed/ ${http}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`)
+    }
